409caac8104fe36e96deba27fd3d744d4da1d83b
Vaishnavi-Raykar <vaishnaviraykar990@gmail.com>
2024-02-16 21:52:32 +0530

    3rd commit

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Section1 from "./Components/Section1";
+import Section6 from "./Components/Section6";
+import Section1_inside1 from "./Components/Section1_inside1";
+import CardUI from "./Components/CardUI";
+
+
+
+const Project = () => {
+  
+  return (
+     <div>
+       <Section1 />
+       <Section6 />
+      <Section1_inside1 />
+      <CardUI />
+    </div>
+  );
+};
+
+export default Project;
